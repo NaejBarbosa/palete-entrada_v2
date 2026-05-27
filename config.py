e8f422c0594b6f401e112d2f86da7ee2ef6d18b1
JEAN BARBOSA <40298383+NaejBarbosa@users.noreply.github.com>
2026-05-27 17:16:49 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,58 @@
+# config.py
+# Arquivo de configuração centralizada
+
+# Configurações de Câmaras
+CAMARAS = [
+    "Resfriados 1",
+    "Resfriados 2",
+    "Congelados 1",
+    "Congelados 2"
+]
+
+# Lista de Vagas
+VAGAS = [
+    "A10D","A10E","A11D","A11E","A12D","A12E","A13D","A13E",
+    "A20D","A20E","A21D","A21E","A22D","A22E","A23D","A23E",
+    "A30D","A30E","A31D","A31E","A32D","A32E","A33D","A33E",
+    "A40D","A40E","A41D","A41E","A42D","A42E","A43D","A43E",
+    "A50D","A50E","A51D","A51E","A52D","A52E","A53D","A53E",
+    "B10D","B10E","B11D","B11E","B12D","B12E","B13D","B13E",
+    "B20D","B20E","B21D","B21E","B22D","B22E","B23D","B23E",
+    "B30D","B30E","B31D","B31E","B32D","B32E","B33D","B33E",
+    "B40D","B40E","B41D","B41E","B42D","B42E","B43D","B43E",
+    "B50D","B50E","B51D","B51E","B52D","B52E","B53D","B53E"
+]
+
+# Opções de Marca
+MARCA_OPCOES = [
+    "",
+    "Seara",
+    "Seara | Doriana",
+    "Seara | Primor",
+    "Seara | Excelsior",
+    "Seara | Macedo",
+    "Seara | Rezende (pizza)",
+    "Lar",
+    "BRF | Perdigão",
+    "BRF | Sadia",
+    "BRF | Claybom",
+    "BRF | Qualy",
+    "BRF | Becel",
+    "Aurora",
+    "Aurora | Peperi",
+    "Aurora | Nobre",
+    "Outro"
+]
+
+# ID da Planilha Google
+SHEET_ID = "1HoN-VLyO5y9wJ4NKdpz42-BljRzT4VeJVY-Wio4CO6g"
+
+# Colunas da Planilha
+COLUNAS_CORRETAS = [
+    "registro",
+    "camara",
+    "camara-vaga",
+    "produto-marca",
+    "produto-descricao",
+    "validade"
+]
